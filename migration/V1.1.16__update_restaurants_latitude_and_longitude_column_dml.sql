1989477099b43ef7ee1c69c7d37f3463b7380385
Yukitaka Maeda <yumaeda@gmail.com>
2020-10-12 17:11:08 +0900

    SQL for update latitude and longitude of the restaurants in Oyama

---
+++ migration/V1.1.16__update_restaurants_latitude_and_longitude_column_dml.sql
@@ -0,0 +1,7 @@
+UPDATE restaurants SET latitude='35.74801113528287', longitude='139.7036661662692' WHERE UuidToBin('146dab66-0526-11eb-ba65-065a10bcba76') = id;
+UPDATE restaurants SET latitude='35.74687023865501', longitude='139.70574118508995' WHERE UuidToBin('cf52fa93-07b2-11eb-ba65-065a10bcba76') = id;
+UPDATE restaurants SET latitude='35.749182110869', longitude='139.70307782788342' WHERE UuidToBin('146dac04-0526-11eb-ba65-065a10bcba76') = id;
+UPDATE restaurants SET latitude='35.749009239907906', longitude='139.7041332460616' WHERE UuidToBin('146dacab-0526-11eb-ba65-065a10bcba76') = id;
+UPDATE restaurants SET latitude='35.749476036115894', longitude='139.70695915867313' WHERE UuidToBin('146daa8f-0526-11eb-ba65-065a10bcba76') = id;
+UPDATE restaurants SET latitude='35.749392165905995', longitude='139.7075149497096' WHERE UuidToBin('146dad3f-0526-11eb-ba65-065a10bcba76') = id;
+UPDATE restaurants SET latitude='35.749499922415914', longitude='139.70695721329045' WHERE UuidToBin('146dadc7-0526-11eb-ba65-065a10bcba76') = id;
