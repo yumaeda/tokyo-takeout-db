4ddd7e12227db0fe0668c315dbaf5ba7f56b6131
Yukitaka Maeda <yumaeda@gmail.com>
2020-10-14 16:53:27 +0900

    SQL for updating latitide and longitude of some shops

---
+++ migration/V1.1.19__update_restaurants_latitude_and_longitude_column_dml.sql
@@ -0,0 +1,8 @@
+UPDATE restaurants SET latitude='35.7466268017912', longitude='139.71744397464974' WHERE UuidToBin('da04f696-ffb0-11ea-ba65-065a10bcba76') = id;
+UPDATE restaurants SET latitude='35.7457266386604', longitude='139.7204510850677' WHERE UuidToBin('da04f762-ffb0-11ea-ba65-065a10bcba76') = id;
+UPDATE restaurants SET latitude='35.76468908983554', longitude='139.70459448795762' WHERE UuidToBin('cf52fcae-07b2-11eb-ba65-065a10bcba76') = id;
+UPDATE restaurants SET latitude='35.719459042241446', longitude='139.72705637163835' WHERE UuidToBin('cf52fda9-07b2-11eb-ba65-065a10bcba76') = id;
+UPDATE restaurants SET latitude='35.73410891449947', longitude='139.72993768006182' WHERE UuidToBin('3305a253-0254-11eb-ba65-065a10bcba76') = id;
+UPDATE restaurants SET latitude='35.634082778338275', longitude='139.7202994747884' WHERE UuidToBin('da04256f-ffb0-11ea-ba65-065a10bcba76') = id;
+UPDATE restaurants SET latitude='35.755955038646604', longitude='139.6974805851876' WHERE UuidToBin('146da2dc-0526-11eb-ba65-065a10bcba76') = id;
+UPDATE restaurants SET latitude='35.75668156954039', longitude='139.6957458137399' WHERE UuidToBin('6918c03b-0964-11eb-ba65-065a10bcba76') = id;
