0fff9b1e9a9a6e06a1d0b3b83dba73d5ca95c8b7
Yukitaka Maeda <yumaeda@gmail.com>
2020-10-15 15:14:09 +0900

    SQL for updating latitude and longitude for the shops in Ikebukuro area

---
+++ migration/V1.1.20__update_restaurants_latitude_and_longitude_column_dml.sql
@@ -0,0 +1,9 @@
+UPDATE restaurants SET latitude='35.7253696055081', longitude='139.71598285249365' WHERE UuidToBin('da04f5c9-ffb0-11ea-ba65-065a10bcba76') = id;
+UPDATE restaurants SET latitude='35.73012116753365', longitude='139.7072828007059' WHERE UuidToBin('da04f4ef-ffb0-11ea-ba65-065a10bcba76') = id;
+UPDATE restaurants SET latitude='35.73107980781442', longitude='139.71482296369194' WHERE UuidToBin('da04f9c7-ffb0-11ea-ba65-065a10bcba76') = id;
+UPDATE restaurants SET latitude='35.7328636659668', longitude='139.70808399678592' WHERE UuidToBin('da053b13-ffb0-11ea-ba65-065a10bcba76') = id;
+UPDATE restaurants SET latitude='35.727082707875205', longitude='139.71325022278612' WHERE UuidToBin('da053f6a-ffb0-11ea-ba65-065a10bcba76') = id;
+UPDATE restaurants SET latitude='35.73217448337968', longitude='139.71885507842438' WHERE UuidToBin('69199622-0964-11eb-ba65-065a10bcba76') = id;
+UPDATE restaurants SET latitude='35.731651828097036', longitude='139.7067647177027' WHERE UuidToBin('da04eaf1-ffb0-11ea-ba65-065a10bcba76') = id;
+UPDATE restaurants SET latitude='35.733927040623', longitude='139.71696126612545' WHERE UuidToBin('da04f8fc-ffb0-11ea-ba65-065a10bcba76') = id;
+UPDATE restaurants SET latitude='35.7343793', longitude='139.7182829' WHERE UuidToBin('146da67c-0526-11eb-ba65-065a10bcba76') = id;
