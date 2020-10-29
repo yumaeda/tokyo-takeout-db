ea471cc3c1cd85922044021a685bfaf025447678
Yukitaka Maeda <yumaeda@gmail.com>
2020-10-29 18:34:02 +0900

    SQL for setting business hours

---
+++ migration/V1.1.26__update_restaurants_business_day_info_column_dml.sql
@@ -0,0 +1,61 @@
+/*
+UPDATE restaurants
+   SET business_day_info = '{"1":"","2":"","3":"","4":"","5":"","6":"","7":""}',
+       open_hours = ''
+ WHERE tokyo_takeout.UuidFromBin(id) = 'xxxx';
+*/
+
+UPDATE restaurants
+   SET business_day_info = '{"1":"17:00〜23:00","2":"17:00〜23:00","3":"17:00〜23:00","4":"17:00〜20:30","5":"17:00〜23:00","6":"17:00〜23:00","7":"17:00〜23:00"}',
+       open_hours = ''
+ WHERE tokyo_takeout.UuidFromBin(id) = '146da2dc-0526-11eb-ba65-065a10bcba76';
+
+UPDATE restaurants
+   SET business_day_info = '{"1":"16:00〜23:30","2":"16:00〜23:30","3":"16:00〜23:30","4":"16:00〜23:30","5":"16:00〜23:30","6":"16:00〜23:30","7":"16:00〜23:30"}',
+       open_hours = ''
+ WHERE tokyo_takeout.UuidFromBin(id) = '6918c03b-0964-11eb-ba65-065a10bcba76';
+
+UPDATE restaurants
+   SET business_day_info = '{"1":"11:30～14:00、17:00～22:00","2":"11:30～14:00、17:00～22:00","3":"11:30～14:00、17:00～22:00","5":"11:30～14:00、17:00～22:00","6":"11:30～14:00、17:00～22:00","7":"11:30～14:00、17:00～22:00"}',
+       open_hours = ''
+ WHERE tokyo_takeout.UuidFromBin(id) = 'cf52fcae-07b2-11eb-ba65-065a10bcba76';
+
+UPDATE restaurants
+   SET business_day_info = '{"1":"17:00〜24:00","2":"17:00〜24:00","3":"17:00〜24:00","4":"17:00〜24:00","5":"17:00〜24:00","6":"17:00〜24:00","7":"17:00〜24:00"}',
+       open_hours = ''
+ WHERE tokyo_takeout.UuidFromBin(id) = 'da04f696-ffb0-11ea-ba65-065a10bcba76';
+
+UPDATE restaurants
+   SET business_day_info = '{"1":"16:00～24:00","2":"17:00～25:00","3":"17:00～25:00","4":"17:00～25:00","5":"17:00～25:00","6":"17:00～25:30","7":"16:00～25:30","8":"16:00～24:00"}',
+       open_hours = ''
+ WHERE tokyo_takeout.UuidFromBin(id) = 'da04f762-ffb0-11ea-ba65-065a10bcba76';
+
+UPDATE restaurants
+   SET business_day_info = '{"3":"18:00〜22:30","4":"18:00〜22:30","5":"18:00〜22:30","6":"18:00〜22:30","7":"18:00〜22:30"}',
+       open_hours = ''
+ WHERE tokyo_takeout.UuidFromBin(id) = '3305a253-0254-11eb-ba65-065a10bcba76';
+
+UPDATE restaurants
+   SET business_day_info = '{"2":"11:30～15:00、17:30～23:00","3":"11:30～15:00、17:30～23:00","4":"11:30～15:00、17:30～23:00","5":"11:30～15:00、17:30～23:00","6":"11:30～15:00、17:30～23:00","7":"12:00～15:00、17:30～22:30"}',
+       open_hours = ''
+ WHERE tokyo_takeout.UuidFromBin(id) = 'cf526b35-07b2-11eb-ba65-065a10bcba76';
+
+UPDATE restaurants
+   SET business_day_info = '{"1":"13:00～21:00","2":"17:00～23:00","4":"17:00～23:00","5":"17:00～23:00","6":"17:00～23:00","7":"17:00～23:00"}',
+       open_hours = ''
+ WHERE tokyo_takeout.UuidFromBin(id) = '69199969-0964-11eb-ba65-065a10bcba76';
+
+UPDATE restaurants
+   SET business_day_info = '{"2":"11:30〜13:30、17:00〜23:00","3":"11:30〜13:30、17:00〜23:00","4":"11:30〜13:30、17:00〜23:00","5":"11:30〜13:30、17:00〜23:00","6":"11:30〜13:30、17:00〜23:00","7":"17:00〜23:00"}',
+       open_hours = ''
+ WHERE tokyo_takeout.UuidFromBin(id) = '6919983c-0964-11eb-ba65-065a10bcba76';
+
+UPDATE restaurants
+   SET business_day_info = '{"1":"16:30～23:00","2":"16:30～23:00","3":"16:30～23:00","4":"16:30～23:00","5":"16:30～23:00","6":"16:30～23:00"}',
+       open_hours = ''
+ WHERE tokyo_takeout.UuidFromBin(id) = '32fe0cfd-0254-11eb-ba65-065a10bcba76';
+
+UPDATE restaurants
+   SET business_day_info = '{"1":"11:00～14:00、17:00～24:00","3":"11:00～14:00、17:00～24:00","4":"11:00～14:00、17:00～24:00","5":"11:00～14:00、17:00～24:00","6":"11:00～14:00、17:00～24:00","7":"11:00～14:00、17:00～24:00"}',
+       open_hours = ''
+ WHERE tokyo_takeout.UuidFromBin(id) = '9186ca10-12a7-11eb-ba65-065a10bcba76';
